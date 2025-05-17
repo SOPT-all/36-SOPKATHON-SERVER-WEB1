062336c05c69449a3f7b2344aa7ab30eae68ba25
choyeongju <jjudy1301@naver.com>
2025-05-18 02:33:32 +0900

    [feat] #12 예외 핸들링 추가

---
+++ src/main/java/sopt/web1/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package sopt.web1.exception;
+
+import sopt.web1.code.ErrorCode;
+
+public class NotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public NotFoundException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
