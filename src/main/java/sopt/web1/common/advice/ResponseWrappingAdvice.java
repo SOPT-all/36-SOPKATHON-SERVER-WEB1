feebc2965f21d50e26a3fa13b97d8eb41cf65620
choyeongju <jjudy1301@naver.com>
2025-05-17 12:57:25 +0900

    [feat] #1 공통 응답 형식 세팅

---
+++ src/main/java/sopt/web1/common/advice/ResponseWrappingAdvice.java
@@ -0,0 +1,36 @@
+package sopt.web1.common.advice;
+
+import lombok.NonNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import sopt.web1.code.SuccessCode;
+import sopt.web1.response.ResponseDto;
+
+public class ResponseWrappingAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, @NonNull Class converterType) {
+        return !(returnType.getParameterType() == ResponseDto.class)
+                && MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            @NonNull MethodParameter returnType,
+            @NonNull MediaType selectedContentType,
+            @NonNull Class selectedConverterType,
+            @NonNull ServerHttpRequest request,
+            @NonNull ServerHttpResponse response
+    ) {
+        if (body instanceof ResponseEntity || body instanceof ResponseDto<?>) {
+            return body;
+        }
+
+        return ResponseDto.success(SuccessCode.OK, body);
+    }
+}
