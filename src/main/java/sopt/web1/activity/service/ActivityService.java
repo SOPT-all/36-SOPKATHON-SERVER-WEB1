a7c01dd694ba184a3ee09d2f83454a7490104bcb
choyeongju <jjudy1301@naver.com>
2025-05-18 02:46:05 +0900

    [feat] #14 놀거리 상세조회 GET API 구현

--- src/main/java/sopt/web1/activity/service/ActivityService.java
+++ src/main/java/sopt/web1/activity/service/ActivityService.java
@@ -1,7 +1,32 @@
 package sopt.web1.activity.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sopt.web1.activity.domain.ActivityEntity;
+import sopt.web1.activity.dto.ActResDto;
+import sopt.web1.activity.repository.ActivityRepository;
+import sopt.web1.code.ErrorCode;
+import sopt.web1.exception.NotFoundException;
 
 @Service
+@RequiredArgsConstructor
 public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActResDto getDetails(
+            final long restaurantId
+    ){
+        ActivityEntity a = activityRepository.findById(restaurantId)
+                .orElseThrow(()->new NotFoundException(ErrorCode.NOT_FOUND_ENTITY));
+        return ActResDto.builder()
+                .name(a.getName())
+                .imageUrl(a.getImageUrl())
+                .address(a.getAddress())
+                .phone(a.getPhone())
+                .time(a.getTime())
+                .description(a.getDescription())
+                .introduction(a.getIntroduction())
+                .build();
+    }
 }
