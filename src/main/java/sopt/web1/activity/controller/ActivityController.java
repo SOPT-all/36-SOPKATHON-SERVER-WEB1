8409691721abc8849c6cdeefcb52c058eb380242
choyeongju <jjudy1301@naver.com>
2025-05-18 02:45:46 +0900

    [feat] #14 url path 수정

--- src/main/java/sopt/web1/activity/controller/ActivityController.java
+++ src/main/java/sopt/web1/activity/controller/ActivityController.java
@@ -1,7 +1,25 @@
 package sopt.web1.activity.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.web1.activity.dto.ActResDto;
+import sopt.web1.activity.service.ActivityService;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class ActivityController {
+
+    private final ActivityService activityService;
+
+    @GetMapping("/pick/activity/{activityId}")
+    public ResponseEntity<ActResDto> getDetails(
+            @PathVariable final Long activityId
+    ){
+        return ResponseEntity.ok(activityService.getDetails(activityId));
+    }
 }
